58e9eb963cef72dbb3eb701d0cd51be5223bc856
Marco Mameli <marco.1.mameli@icloud.com>
2025-02-19 18:30:01 +0100

    Update chat_gradio.py

--- pointllm/eval/chat_gradio.py
+++ pointllm/eval/chat_gradio.py
@@ -144,7 +144,11 @@ def start_conversation(args, model, tokenizer, point_backbone_config, keywords, 
                 color_data = color_data.astype(np.float16) / 255 # model input is (0-1)
             else:
                 colors = color_data.astype(np.float32) / 255 # model input is (0-1)
-
+                
+            print('Info colori:')
+            print(len(colors) + '\n')
+            print(colors.shape + '\n')
+            print(colors)
             # Convert the RGB color data to a list of RGB strings in the format 'rgb(r, g, b)'
             color_strings = ['rgb({},{},{})'.format(r, g, b) for r, g, b in color_data]
 
